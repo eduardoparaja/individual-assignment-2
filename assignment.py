1b3b7f47b6e80e3d3c58fc7e1e699c98bead405e
eduardoparaja <44499403+eduardoparaja@users.noreply.github.com>
2018-11-09 19:01:54 +0100

    add bubble sort function

--- assignment.py
+++ assignment.py
@@ -5,3 +5,11 @@ Created on Fri Nov  9 18:47:36 2018
 @author: edup_
 """
 
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
